e4b3bc44765f666a16adafb956f1821ee0ef40ce
Kevin Hardy <kehardy12@unm.edu>
2019-08-28 19:44:18 -0600

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ( ) {
+	std::cout << "Hello Class" << std::endl;
+	std::cout << "My name is Kevin Hardy and I really hope to gain programming skills" 
+		<< std::endl;
+	std::cout << "I am from a small town called Navajo, New Mexico" << std::endl;
+
+return 0;
+}
---
+++ hello.md
@@ -0,0 +1,3 @@
+Hello Class
+My name is Kevin Hardy and I really hope to gain programming skills
+I am from a small town called Navajo, New Mexico
